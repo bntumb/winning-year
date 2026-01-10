8441e86664f5d42d93aa59409d089614345333ea
Ben-Elie <51305376+bntumb@users.noreply.github.com>
2026-01-10 14:15:22 +0000

    Add button style and story

---
+++ src/design-system/compoments/AddButton/AddButton.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import "./styles.css"
+const AddButton =()=>{
+    return(
+        <div className='addButton'>
+             +
+        </div>
+        )
+}
+
+ export default AddButton
---
+++ src/stories/AddButton.stories.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import AddButton from '@/design-system/compoments/AddButton/AddButton';
+export default {
+  title: 'Componenets/Button',
+  component: AddButton,
+};
+
+export const Default = () => <AddButton />;
