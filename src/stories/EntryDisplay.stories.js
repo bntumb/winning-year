c5099088e3c38c90a73b49f71961cbf32f79185c
Ben-Elie <51305376+bntumb@users.noreply.github.com>
2026-01-10 15:01:16 +0000

    added story

---
+++ src/stories/EntryDisplay.stories.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import EntryDisplay from '@/design-system/compoments/EntryDisplay/EntryDisplay';
+export default {
+  title: 'Componenets/EntryDisplay',
+  component: EntryDisplay,
+};
+
+export const Default = () => <EntryDisplay />;
