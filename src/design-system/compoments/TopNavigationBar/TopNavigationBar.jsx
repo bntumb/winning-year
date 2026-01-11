09dd799b11f719f709f779c256aad3fd581e9c21
Ben-Elie <51305376+bntumb@users.noreply.github.com>
2026-01-11 16:10:27 +0000

    navigation bar  component

---
+++ src/design-system/compoments/TopNavigationBar/TopNavigationBar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import {NotificationButton} from "@/design-system/assets/icons/icons.js";
+import LinkWithIcon from '../LinkWithIcons/LinkWithIcons';
+import './styles.css'
+
+function TopNavigationBar() {
+  return (
+    <div>
+        <div className="topNavigationBar">
+            <div className={'iconContainer logo'}>
+                <LinkWithIcon linkText={'Winning Year'} />
+            </div>  
+            <div className={'iconContainer'}>
+                <NotificationButton/>
+            </div>  
+        </div>
+    </div>
+  )
+}
+
+export default TopNavigationBar
