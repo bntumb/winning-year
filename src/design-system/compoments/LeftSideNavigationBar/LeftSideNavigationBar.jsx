4c5c6f8693fdfcfa75f91388986d1683027afe81
Ben-Elie <51305376+bntumb@users.noreply.github.com>
2026-01-18 11:02:30 +0000

    added new menu items and updated the styling

--- src/design-system/compoments/LeftSideNavigationBar/LeftSideNavigationBar.jsx
+++ src/design-system/compoments/LeftSideNavigationBar/LeftSideNavigationBar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {NotificationButton} from "@/design-system/assets/icons/icons.js";
 import LinkWithIcon from '../LinkWithIcons/LinkWithIcons';
 import './styles.css'
-import { HomeIcon, SettingsIcon } from '@/design-system/assets/icons/icons.js'
+import { HomeIcon, SettingsIcon, DashboardIcon, DiscoverIcon } from '@/design-system/assets/icons/icons.js'
 
 function LeftSideNavigationBar () {
   return (
@@ -13,11 +13,22 @@ function LeftSideNavigationBar () {
             </div>  
             <div className="navContainter">
                 <div className="top">
+                  <div className="menuItem">
                     <LinkWithIcon icon={<HomeIcon/>} linkText={'My Wins'} />
+                  </div>
                 </div>
                 <div className="center">
-                  <LinkWithIcon linkText={'Meetings'} />
-                </div>
+         
+
+                <div className="menuItem">
+                  <LinkWithIcon  icon={<DashboardIcon/>}linkText={'Dashboard'} />
+
+                  </div>
+         <div className="menuItem">
+                    
+                  <LinkWithIcon  icon={<DiscoverIcon/>}linkText={'Discover'} />
+                  </div>
+                             </div>
                 <div className="bottom">
                     <LinkWithIcon icon={<SettingsIcon/>} linkText={'Settings'} />
                 </div>
